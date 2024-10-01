ec7cc6a4b3539cadbcb890e94cedfbcda5cb5d25
SoladrinHub <cointoken8021@gmail.com>
2024-09-30 23:40:21 -0700

    function taskFirst to instantiate variables using const

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -4,13 +4,12 @@ export function taskFirst() {
 }
 
 export function getLast() {
-  return ' is okay.';
+  return ' is okay';
 }
 
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast(); // This will result in: "But sometimes let is okay."
+  combination += getLast();
 
   return combination;
 }
-
