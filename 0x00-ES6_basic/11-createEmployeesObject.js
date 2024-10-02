4672d375cb54740c7b10511a7f0c01ba2193bb22
SoladrinHub <cointoken8021@gmail.com>
2024-10-02 01:17:58 -0700

    Write a function named createEmployeesObject that will receive two arguments:

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,6 @@
-function createEmployeesObject(departmentName, employees) {
+export default function createEmployeesObject(departmentName, employees) {
+  // Create an object with the departmentName as the key and employees as the value
   return {
-    [departmentName]: employees
+    [departmentName]: employees,
   };
 }
